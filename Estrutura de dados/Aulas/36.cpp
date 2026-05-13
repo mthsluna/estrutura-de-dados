0ffd8103be933457d118afd519559c0f7364cb4c
Matheus Luna <mthsluna@hotmail.com>
2026-05-13 08:56:58 -0300

    Add files via upload

---
+++ Estrutura de dados/Aulas/36.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Fila {
+public:
+    Fila() {}
+
+    void enfileirar(int elemento) {
+        lista.push_back(elemento);
+    }
+
+    int desenfileirar() {
+        if (lista.empty()) {
+            return -1;
+        }
+
+        int elemento = lista.front();
+        lista.pop_front();
+        return elemento;
+    }
+
+    bool vazia() {
+        return lista.empty();
+    }
+
+private:
+    list<int> lista;
+};
+
+int main() {
+    Fila fila;
+
+    fila.enfileirar(1);
+    fila.enfileirar(2);
+    fila.enfileirar(3);
+    fila.desenfileirar();
+
+    return 0;
+}
